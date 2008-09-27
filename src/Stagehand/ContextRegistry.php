379e1c50df8c3f9f041391cbc120cc3d2d4c8164
KUBO Atsuhiro <kubo@iteman.jp>
2008-09-27 10:00:56 +0000

    - Added the Stagehand_ContextRegistry package.

---
+++ src/Stagehand/ContextRegistry.php
@@ -0,0 +1,172 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP version 5
+ *
+ * Copyright (c) 2008 KUBO Atsuhiro <iteman@users.sourceforge.net>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Stagehand_ContextRegistry
+ * @copyright  2008 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @since      File available since Release 0.1.0
+ */
+
+namespace Stagehand;
+
+use Stagehand::IContext;
+
+// {{{ Stagehand::ContextRegistry
+
+/**
+ * A reusable context registry.
+ *
+ * @package    Stagehand_ContextRegistry
+ * @copyright  2008 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @since      Class available since Release 0.1.0
+ */
+abstract class ContextRegistry
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access protected
+     */
+
+    protected static $contextStack = array();
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    // }}}
+    // {{{ getContext()
+
+    /**
+     * Gets the current context.
+     *
+     * @return mixed
+     */
+    public static function getContext()
+    {
+        if (!count(static::$contextStack)) {
+            return;
+        }
+
+        return static::$contextStack[ count(static::$contextStack) - 1 ];
+    }
+
+    // }}}
+    // {{{ clear()
+
+    /**
+     * Clear all context objects in the registry.
+     */
+    public static function clear()
+    {
+        foreach (static::$contextStack as $context) {
+            $context->clear();
+        }
+
+        static::$contextStack = array();
+    }
+
+    // }}}
+    // {{{ setContext()
+
+    /**
+     * Sets a context object as the current context.
+     *
+     * @param Stagehand::IContext $context
+     */
+    public static function setContext(IContext $context)
+    {
+        array_push(static::$contextStack, $context);
+    }
+
+    // }}}
+    // {{{ restoreCacheDirectory()
+
+    /**
+     * Restores the previous context.
+     */
+    public static function restoreContext()
+    {
+        $context = static::getContext();
+        if (is_null($context)) {
+            return;
+        }
+
+        $context->clear();
+        array_pop(static::$contextStack);
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access protected
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
